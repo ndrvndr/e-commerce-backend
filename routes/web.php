2d6143e33477f88b9e0d5debb53d20c1a8bf6789
ndrvndr <andreavindra37@gmail.com>
2026-05-08 09:24:45 +0700

    feat(routes): redirect root path to admin login

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\Auth\SocialiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin/login');
 
 Route::get('/auth/{provider}/redirect', [SocialiteController::class, 'redirectToProvider']);
 Route::get('/auth/{provider}/callback', [SocialiteController::class, 'handleProviderCallback']);
