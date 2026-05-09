f6f014e35a1d5dcaf88bf1c709697867b38d1b1e
ndrvndr <andreavindra37@gmail.com>
2026-05-09 22:23:15 +0700

    fix(app): force https scheme in production to resolve mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Filament\Forms\Components\FileUpload;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         FileUpload::configureUsing(function (FileUpload $fileUpload): void {
             $fileUpload
                 ->disk('s3')
